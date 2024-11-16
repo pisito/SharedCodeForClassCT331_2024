c2aa702005ab675435d3a711a7e01ddf1a79662f
Pisit <pisit.praiwattana@gmail.com>
2024-11-16 09:58:23 +0700

    Update HPSystem.cs
    Update HP System to have Max HP

--- PisitShareFolder/Scripts/Pisit/HPSystem.cs
+++ PisitShareFolder/Scripts/Pisit/HPSystem.cs
@@ -8,11 +8,18 @@ namespace Pisit
     public class HPSystem : MonoBehaviour
     {
         public float HP;
+        public float MaxHP = 100f;
 
         public void ModifyHP( float value )
         {
             HP = HP + value;
-            Mathf.Clamp(HP, 0.0f, 100.0f);
+            // Mathf.Clamp(HP, 0.0f, 100.0f);
+            Mathf.Clamp(HP, 0.0f, MaxHP);
+        }
+
+        public void SetHPToMax()
+        {
+            HP = MaxHP;
         }
     }
 }
